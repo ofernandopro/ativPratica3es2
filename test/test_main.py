9d632fdde5be8cc4e865e26596dc7538607dcef4
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-06-13 21:47:09 -0300

    Consertando erro de referência a arquivos

--- test/test_main.py
+++ test/test_main.py
@@ -7,10 +7,10 @@ def test_add():
     assert main.add(-10, -12) == -22
 
 def test_sub():
-    assert main2.sub(3, 4) == -1
-    assert main2.sub(4.5, 4) == 0
-    assert main2.sub(3.9, 4) == -1
-    assert main2.sub(4.2, 3.8) == 0
+    assert main.sub(3, 4) == -1
+    assert main.sub(4.5, 4) == 0
+    assert main.sub(3.9, 4) == -1
+    assert main.sub(4.2, 3.8) == 0
 
 def test_to_sentence():
     assert main.to_sentence('engenharia') == 'Engenharia.'
